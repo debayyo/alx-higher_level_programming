d343740b52dd40e218a319c24354164237ca0a40
debayyo <adegbitequadriadebayo@gmail.com>
2022-06-02 14:31:16 -0700

    m

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    argc = len(sys.argv)
+    sum = 0
+    for i in range(1, argc):
+        sum += int(sys.argv[i])
+    print(sum)
+
+if __name__ == "__main__":
+    main()
